f0317d32082fe41d955ad68aa11bd1c237fc5b09
wilfried bauer <wilfried.bauer@gmail.com>
2023-12-23 03:43:42 +0100

    finally got breadcrumbs to work with barbecue.nvim. navic is used inside, but thank god i dont have to set it up

--- init.lua
+++ init.lua
@@ -623,6 +623,18 @@ require('lazy').setup({
   'nvim-tree/nvim-tree.lua',
   'nvim-treesitter/nvim-treesitter-context',
   {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
+  },
+  {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
