c5e67fff20e64b18faad02dc816df0c741c93105
wilfried bauer <wilfried.bauer@gmail.com>
2024-03-25 10:54:07 +0100

    fixed fold levels showing up in statuscolumn. this should fix the fold level numbers showing after the first install of the new version of the config with statuscolumn changes. Previously, the foldlevelicon positon had to be switched in the statuscol.nvim plugin (rearrange order of columns in statuscolumn, then change back)

--- init.lua
+++ init.lua
@@ -900,6 +900,8 @@ require('lazy').setup({
         config = function()
           local builtin = require("statuscol.builtin")
           require("statuscol").setup({
+            foldfunc = "builtin",
+            setopt = true,
             relculright = true,
             segments = {
               { text = { "%s" },                  click = "v:lua.ScSa" },
