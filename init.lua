facd977a0cc67349b84a4d677071d9a0ef41f6f5
wilfried bauer <wilfried.bauer@gmail.com>
2024-04-13 22:07:53 +0200

    added dotnet debugger and test runner support. everything works now! :) :)

--- init.lua
+++ init.lua
@@ -652,13 +652,7 @@ require('lazy').setup({
       }
       dap.adapters.coreclr = {
         type = "executable",
-        command = "/usr/bin/netcoredbg",
-        args = { "--interpreter=vscode" },
-      }
-
-      dap.adapters.netcoredbg = {
-        type = "executable",
-        command = "/usr/bin/netcoredbg",
+        command = "netcoredbg",
         args = { "--interpreter=vscode" },
       }
 
@@ -673,6 +667,7 @@ require('lazy').setup({
         },
       }
 
+      vim.o.noshellslash = true
 
       -- Basic debugging keymaps, feel free to change to your liking!
       vim.keymap.set('n', '<leader>bc', dap.continue, { desc = '[B]ug: Start/[C]ontinue' })
@@ -955,9 +950,11 @@ require('lazy').setup({
   {
     "nvim-neotest/neotest",
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter"
+      "nvim-treesitter/nvim-treesitter",
+      "Issafalcon/neotest-dotnet",
     }
   },
   { "lukas-reineke/virt-column.nvim", opts = {} },
@@ -1134,6 +1131,35 @@ require('lazy').setup({
 }, {})
 
 -- [[Setup Custom Plugins ]]
+
+require("neotest").setup({
+  adapters = {
+    require("neotest-dotnet")({
+      dap = {
+        -- Extra arguments for nvim-dap configuration
+        -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+        args = { justMyCode = false },
+        -- Enter the name of your dap adapter, the default value is netcoredbg
+        adapter_name = "netcoredbg"
+      },
+      -- Let the test-discovery know about your custom attributes (otherwise tests will not be picked up)
+      -- Note: Only custom attributes for non-parameterized tests should be added here. See the support note about parameterized tests
+      custom_attributes = {
+        -- xunit = { "MyCustomFactAttribute" },
+        -- nunit = { "MyCustomTestAttribute" },
+        -- mstest = { "MyCustomTestMethodAttribute" }
+      },
+      -- Provide any additional "dotnet test" CLI commands here. These will be applied to ALL test runs performed via neotest. These need to be a table of strings, ideally with one key-value pair per item.
+      dotnet_additional_args = {
+        "--verbosity detailed"
+      },
+      -- Tell neotest-dotnet to use either solution (requires .sln file) or project (requires .csproj or .fsproj file) as project root
+      -- Note: If neovim is opened from the solution root, using the 'project' setting may sometimes find all nested projects, however,
+      --       to locate all test projects in the solution more reliably (if a .sln file is present) then 'solution' is better.
+      discovery_root = "project" -- Default
+    })
+  }
+})
 
 require("eagle").setup({
   -- override the default values found in config.lua
@@ -1358,10 +1384,17 @@ vim.keymap.set('n', '<leader>j', require('treesj').toggle, { desc = "Toggle Join
 vim.keymap.set('n', '<C-p>', ':BufferLineCyclePrev<CR>', { desc = 'Previous Tab' })
 vim.keymap.set('n', '<C-n>', ':BufferLineCycleNext<CR>', { desc = 'Next Tab' })
 
-vim.keymap.set('n', '<leader>TT', '<cmd>lua require("neotest").run.run()<CR>', { desc = '[T]est: Run nearest [T]est' })
-vim.keymap.set('n', '<leader>TF', '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
+vim.keymap.set('n', '<leader>Ts', '<cmd>Neotest summary toggle<CR>', { desc = '[T]est: [S]ummary toggle' })
+vim.keymap.set('n', '<leader>TT', '<cmd>Neotest output toggle<CR>', { desc = '[T]est: Output line toggle' })
+vim.keymap.set('n', '<leader>Tt', '<cmd>Neotest output-panel toggle<CR>', { desc = '[T]est: Output panel toggle' })
+vim.keymap.set('n', '<leader>Tn', '<cmd>lua require("neotest").run.run()<CR>', { desc = '[T]est: Run [N]earest Test' })
+vim.keymap.set('n', '<leader>Tf', '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
   { desc = '[T]est: Run [F]ile' })
-vim.keymap.set('n', '<leader>TS', '<cmd>lua require("neotest").run.stop()<CR>', { desc = '[T]est: [S]top' })
+vim.keymap.set('n', '<leader>Ta', '<cmd>lua require("neotest").run.attach()<CR>',
+  { desc = '[T]est: [A]ttach to nearest [T]est' })
+vim.keymap.set('n', '<leader>Tb', '<cmd>lua require("neotest").run.run({strategy = "dap"})<CR>',
+  { desc = '[T]est: De[b]ug nearest Test' })
+vim.keymap.set('n', '<leader>Ts', '<cmd>lua require("neotest").run.stop()<CR>', { desc = '[T]est: [S]top' })
 
 vim.keymap.set('n', '<leader>gdd', ':DiffviewOpen<cr>', { desc = '[G]it [D]iff View' })
 vim.keymap.set('n', '<leader>gdo', ':DiffviewOpen ', { desc = '[G]it [D]iff View [O]pen' })
