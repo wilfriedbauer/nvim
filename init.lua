02993faedb51ea6be10713a08b7ad061dd63bd8d
wilfried bauer <wilfried.bauer@gmail.com>
2024-06-08 09:39:28 +0200

    q now closes some special buffers.

--- init.lua
+++ init.lua
@@ -200,6 +200,22 @@ vim.diagnostic.handlers.signs = {
   end,
 }
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "checkhealth",
+    "fugitive*",
+    "git",
+    "help",
+    "lspinfo",
+    "netrw",
+    "notify",
+    "qf",
+    "query",
+  },
+  callback = function()
+    vim.keymap.set("n", "q", vim.cmd.close, { desc = "Close the current buffer", buffer = true })
+  end,
+})
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
