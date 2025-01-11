491b0d6e6540035a86b6dd1aad2c648118cca3f6
Wilfried Bauer <bw.bauerwilfried@gmail.com>
2025-01-11 21:20:30 +0100

    added h,H,l and L keymaps to nvim-tree.

--- init.lua
+++ init.lua
@@ -1150,9 +1150,57 @@ require("lazy").setup({
       },
     },
     config = function()
+      local function my_on_attach(bufnr)
+        local api = require("nvim-tree.api")
+
+        local function edit_or_open()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file
+            api.node.open.edit()
+            -- Close the tree if file was opened
+            api.tree.close()
+          end
+        end
+
+        -- open as vsplit on current node
+        local function vsplit_preview()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file as vsplit
+            api.node.open.vertical()
+          end
+
+          -- Finally refocus on tree if it was lost
+          api.tree.focus()
+        end
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- on_attach
+        vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+        vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+        vim.keymap.set("n", "h", api.tree.close, opts("Close"))
+        vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+      end
+
       -- vim.g.loaded_netrw = 1
       -- vim.g.loaded_netrwPlugin = 1
       require("nvim-tree").setup({
+        on_attach = my_on_attach,
         sort = {
           sorter = "case_sensitive",
         },
@@ -1576,6 +1624,7 @@ require("lazy").setup({
           "debugpy",
           "python-lsp-server",
           "lua-language-server",
+          "angular-language-server",
           "csharpier",
           "omnisharp",
           "netcoredbg",
