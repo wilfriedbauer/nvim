a38ccfc3f4deb99f6f01581ac77bf3348f714e8c
Wilfried Bauer <bw.bauerwilfried@gmail.com>
2025-08-29 11:18:07 +0200

    added lsp overload navigation. lazy update.

--- init.lua
+++ init.lua
@@ -477,6 +477,10 @@ require("lazy").setup({
       { "mason-org/mason-lspconfig.nvim" },
       { "mason-org/mason.nvim" },
       {
+        'Issafalcon/lsp-overloads.nvim',
+        opts = {},
+      },
+      {
         "folke/lazydev.nvim",
         ft = "lua", -- only load on lua files
         opts = {
@@ -524,131 +528,18 @@ require("lazy").setup({
             require("conform").format({ async = false, lsp_fallback = true })
             vim.cmd("retab")
           end, { desc = "Format current buffer with LSP" })
+
+          vim.keymap.set("i", "<A-s>", "<cmd>LspOverloadsSignature<CR>", { desc = "Lsp Overloads Signature", noremap = true, silent = true, buffer = bufnr })
+          vim.keymap.set("n", "<A-s>", "<cmd>LspOverloadsSignature<CR>", { desc = "Lsp Overloads Signature", noremap = true, silent = true, buffer = bufnr })
         end,
       })
     end,
   },
   {
     "GustavEikaas/easy-dotnet.nvim",
-    -- 'nvim-telescope/telescope.nvim' or 'ibhagwan/fzf-lua' or 'folke/snacks.nvim'
-    -- are highly recommended for a better experience
     dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
     config = function()
-      local function get_secret_path(secret_guid)
-        local path = ""
-        local home_dir = vim.fn.expand('~')
-        if require("easy-dotnet.extensions").isWindows() then
-          local secret_path = home_dir ..
-              '\\AppData\\Roaming\\Microsoft\\UserSecrets\\' .. secret_guid .. "\\secrets.json"
-          path = secret_path
-        else
-          local secret_path = home_dir .. "/.microsoft/usersecrets/" .. secret_guid .. "/secrets.json"
-          path = secret_path
-        end
-        return path
-      end
-
-      local dotnet = require("easy-dotnet")
-      -- Options are not required
-      dotnet.setup({
-        ---@type TestRunnerOptions
-        test_runner = {
-          ---@type "split" | "vsplit" | "float" | "buf"
-          viewmode = "float",
-          ---@type number|nil
-          vsplit_width = nil,
-          ---@type string|nil "topleft" | "topright" 
-          vsplit_pos = nil,
-          enable_buffer_test_execution = true, --Experimental, run tests directly from buffer
-          noBuild = true,
-            icons = {
-              passed = "",
-              skipped = "",
-              failed = "",
-              success = "",
-              reload = "",
-              test = "",
-              sln = "󰘐",
-              project = "󰘐",
-              dir = "",
-              package = "",
-            },
-          mappings = {
-            run_test_from_buffer = { lhs = "<leader>r", desc = "run test from buffer" },
-            filter_failed_tests = { lhs = "<leader>fe", desc = "filter failed tests" },
-            debug_test = { lhs = "<leader>d", desc = "debug test" },
-            go_to_file = { lhs = "g", desc = "go to file" },
-            run_all = { lhs = "<leader>R", desc = "run all tests" },
-            run = { lhs = "<leader>r", desc = "run test" },
-            peek_stacktrace = { lhs = "<leader>p", desc = "peek stacktrace of failed test" },
-            expand = { lhs = "o", desc = "expand" },
-            expand_node = { lhs = "E", desc = "expand node" },
-            expand_all = { lhs = "-", desc = "expand all" },
-            collapse_all = { lhs = "W", desc = "collapse all" },
-            close = { lhs = "q", desc = "close testrunner" },
-            refresh_testrunner = { lhs = "<C-r>", desc = "refresh testrunner" }
-          },
-          --- Optional table of extra args e.g "--blame crash"
-          additional_args = {}
-        },
-        new = {
-          project = {
-            prefix = "sln" -- "sln" | "none"
-          }
-        },
-        ---@param action "test" | "restore" | "build" | "run"
-        terminal = function(path, action, args)
-          args = args or ""
-          local commands = {
-            run = function() return string.format("dotnet run --project %s %s", path, args) end,
-            test = function() return string.format("dotnet test %s %s", path, args) end,
-            restore = function() return string.format("dotnet restore %s %s", path, args) end,
-            build = function() return string.format("dotnet build %s %s", path, args) end,
-            watch = function() return string.format("dotnet watch --project %s %s", path, args) end,
-          }
-          local command = commands[action]()
-          if require("easy-dotnet.extensions").isWindows() == true then command = command .. "\r" end
-          vim.cmd("vsplit")
-          vim.cmd("term " .. command)
-        end,
-        secrets = {
-          path = get_secret_path
-        },
-        csproj_mappings = true,
-        fsproj_mappings = true,
-        auto_bootstrap_namespace = {
-            --block_scoped, file_scoped
-            type = "block_scoped",
-            enabled = true,
-            use_clipboard_json = {
-              behavior = "prompt", --'auto' | 'prompt' | 'never',
-              register = "+", -- which register to check
-            },
-        },
-        -- choose which picker to use with the plugin
-        -- possible values are "telescope" | "fzf" | "snacks" | "basic"
-        -- if no picker is specified, the plugin will determine
-        -- the available one automatically with this priority:
-        -- telescope -> fzf -> snacks ->  basic
-        picker = "telescope",
-        background_scanning = true,
-        notifications = {
-          --Set this to false if you have configured lualine to avoid double logging
-          handler = function(start_event)
-            local spinner = require("easy-dotnet.ui-modules.spinner").new()
-            spinner:start_spinner(start_event.job.name)
-            ---@param finished_event JobEvent
-            return function(finished_event)
-              spinner:stop_spinner(finished_event.result.text, finished_event.result.level)
-            end
-          end,
-        },
-        debugger = {
-          mappings = {
-            open_variable_viewer = { lhs = "T", desc = "open variable viewer" },
-          },
-        }
-      })
+      require("easy-dotnet").setup()
     end
   },
   {
@@ -776,7 +667,6 @@ require("lazy").setup({
           { name = "nvim_lsp_signature_help", group_index = 1 },
           { name = "luasnip", group_index = 3 },
           { name = "nvim_lsp", group_index = 1 },
-          { name = 'easy-dotnet', group_index = 2 },
           { name = "nvim_lua", group_index = 2 },
           { name = "path", group_index = 2 },
           { name = "buffer", group_index = 2, keyword_length = 2 },
