6c6d40458e1f5d84a79a8f7b3b8d7a613c88ed89
wil <bw.bauerwilfried@gmail.com>
2025-09-22 16:08:54 +0200

    added kulala.nvim for rest requests (great plugin). lazy and mason update.

--- init.lua
+++ init.lua
@@ -1182,6 +1182,20 @@ require("lazy").setup({
     end,
   },
   {
+    "mistweaverco/kulala.nvim",
+    keys = {
+      { "<leader>Rs", desc = "Send request" },
+      { "<leader>Ra", desc = "Send all requests" },
+      { "<leader>Rb", desc = "Open scratchpad" },
+    },
+    ft = { "http", "rest" },
+    opts = {
+      global_keymaps = true,
+      global_keymaps_prefix = "<leader>R",
+      kulala_keymaps_prefix = "",
+    },
+  },
+  {
     "folke/which-key.nvim",
     event = "UIEnter",
     config = function()
