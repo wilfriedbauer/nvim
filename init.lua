f55c50c8f65a43e33a422edba968b682179bd072
wil <bw.bauerwilfried@gmail.com>
2026-01-27 09:52:35 +0100

    feat: readding blink.cmp and removed built-in completion

--- init.lua
+++ init.lua
@@ -12,34 +12,22 @@ vim.o.showmode = false
 vim.o.cmdheight = 0
 vim.wo.signcolumn = "auto:5"
 vim.o.updatetime = 100
-vim.o.timeoutlen = 500
+vim.o.timeoutlen = 300
 vim.o.title = true
 vim.o.titlestring = [[%f %h%m%r%w %{v:progname} (%{tabpagenr()} of %{tabpagenr('$')}) - %{fnamemodify(getcwd(), ":t")} - %{luaeval("require('dap').status()")}]]
-vim.o.autocomplete = false
-vim.o.complete = "o,.,w,t,b,u"
-vim.o.completeopt = "fuzzy,menuone,noselect,popup"
-vim.o.pumheight = 15
-vim.o.pummaxwidth = 100
-vim.opt.shortmess:prepend("c")
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    vim.bo.complete = "o"
-    vim.lsp.completion.enable(true, ev.data.client_id, ev.buf)
-  end,
-})
 vim.o.confirm = true
 vim.o.termguicolors = true
 vim.o.statuscolumn = "%s %l %C "
 vim.opt.path:append(",**")
 vim.o.list = true
 vim.opt.listchars = {
-  tab = "«-»",
-  trail = "·",
-  nbsp = "␣",
-  leadmultispace = " │",
-  extends = "→",
-  precedes = "←",
-  -- eol = '↵',
+	tab = "«-»",
+	trail = "·",
+	nbsp = "␣",
+	leadmultispace = " │",
+	extends = "→",
+	precedes = "←",
+	-- eol = '↵',
 }
 vim.o.foldmethod = "indent"
 vim.o.foldcolumn = "1"
@@ -611,6 +599,114 @@ require("lazy").setup({
     end,
   },
   {
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'xzbdmw/colorful-menu.nvim',
+      'onsails/lspkind.nvim'
+    },
+
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      -- keymap = { preset = 'super-tab' },
+      keymap = {
+        preset = 'none',
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+        ['<C-y>'] = { 'select_and_accept', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<Tab>'] = {
+          function(cmp)
+            if cmp.snippet_active() then return cmp.accept()
+            else return cmp.select_and_accept() end
+          end,
+          'snippet_forward',
+          'fallback'
+        },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+      },
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      cmdline = { completion = { ghost_text = { enabled = true } } },
+      -- (Default: false) Only show the documentation popup when manually triggered
+      completion = {
+        documentation = { auto_show = true },
+        menu = {
+          draw = {
+            -- We don't need label_description now because label and label_description are already
+            -- combined together in label by colorful-menu.nvim.
+            columns = { { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                    return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                    return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+              kind_icon = {
+                  text = function(ctx)
+                      return require('lspkind').symbol_map[ctx.kind] or ''
+                  end,
+              },
+            },
+          },
+        },
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     branch = "main",
@@ -943,6 +1039,17 @@ require("lazy").setup({
             require("conform").format({ async = false, lsp_fallback = true })
             vim.cmd("retab")
           end, { desc = "Format current buffer with LSP" })
+
+
+          vim.keymap.set({ "n", "x" }, "grf", function()
+              local mode = vim.fn.mode()
+
+              if mode == "v" or mode == "V" or mode == "\22" then
+                  vim.lsp.buf.format({ range = true, async = true })
+              else
+                  vim.lsp.buf.format({ async = true })
+              end
+          end, { desc = "LSP format (buffer or selection)" })
 
           -- Jump to next error
           vim.keymap.set("n", "]e", function()
